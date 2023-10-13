553a0acb609a53c72d34700571f8e062090d02bb
Giova2002 <cianfaglione.giovanna@correo.unimet.edu.ve>
2023-10-12 23:32:33 -0400

    Final

---
+++ src/Interfaz/Spiners.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Interfaz;
+
+import javax.swing.JSpinner;
+
+/**
+ *
+ * @author giovannacianfaglione
+ */
+public class Spiners extends JSpinner {
+
+    public Spiners() {
+        
+        
+       
+    }
+
+    void setMaximumSize(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void setMinimumSize(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+     
+    
+    
+    
+}
